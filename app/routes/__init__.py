f67231df28be33c3ae256aa7f366e981f26045f2
Porter James <porterjames11211@gmail.com>
2019-08-15 17:09:41 +0800

    setting up login form

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,3 @@
-from flask import render_template
-from app import app
+from . import login
+from . import home
 
-
-@app.route('/')
-def index():
-    user = {'username': 'prtr'}
-    return render_template('index.html', user=user)
