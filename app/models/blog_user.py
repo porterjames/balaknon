560dc8bd74aff39dd8de8057b063415697ee14ae
Porter James <porterjames11211@gmail.com>
2019-08-16 20:43:54 +0800

    adding some basic CSS

--- app/models/blog_user.py
+++ app/models/blog_user.py
@@ -1,7 +1,9 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class BlogUser(db.Model):
+class BlogUser(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
@@ -10,3 +12,16 @@ class BlogUser(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        """save hashed password"""
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """check given password against stored hash"""
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(p_id):
+    return BlogUser.query.get(int(p_id))
--- app/routes/login.py
+++ app/routes/login.py
@@ -1,13 +1,30 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
 from app import app
 from app.forms.login import LoginForm
+from app.models.blog_user import BlogUser
+from flask_login import current_user, login_user, logout_user
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me = {}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('home'))
+        user = BlogUser.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
     return render_template('login.html', title='sign in', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
